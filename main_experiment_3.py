0a40df9d3c2edbd1eae5a8d93bb96cf6e8a5bff4
shx-lyu <64817866+shx-lyu@users.noreply.github.com>
2023-07-16 20:54:15 +0800

    Update main_experiment_3.py

--- main_experiment_3.py
+++ main_experiment_3.py
@@ -24,17 +24,23 @@ def BitMapper(m, bits_per_row, rows, cols):
     return M
 
 def L16Decoding(y):
-    n = y.shape[0]
-    xhat1 = DnDecoding(y)
+    G1 = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+                   [1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+                   [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], dtype=int)
 
-    xhat2 = DnDecoding(y - 0.5 * np.ones(n)) + 0.5 * np.ones(n)
-
-    if np.linalg.norm(y - xhat1) <= np.linalg.norm(y - xhat2):
-        xhat = xhat1
-    else:
-        xhat = xhat2
-
-    return xhat
+    n = 16
+    Dist = np.zeros(32)
+    xhat = np.zeros((n, 32), dtype=int)
+    for k in range(32):
+        d = np.dot(G1.transpose(), np.unpackbits(np.array([k], dtype=np.uint8))[-5:])
+        ybar = (y - d) / 2
+        vhat = 2 * DnDecoding(ybar) + d
+        xhat[:, k] = vhat
+        Dist[k] = np.linalg.norm(y - vhat)
+    v_final = xhat[:, np.argmin(Dist)]
+    return v_final
 
 def DnDecoding(y):
     f = np.round(y)
