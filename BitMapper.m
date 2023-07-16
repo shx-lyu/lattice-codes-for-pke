e577f664353a6ea8b679815c314a8e3f04264657
shx-lyu <64817866+shx-lyu@users.noreply.github.com>
2023-07-16 21:33:20 +0800

    Update BitMapper.m

--- BitMapper.m
+++ BitMapper.m
@@ -1,16 +1,14 @@
-function M = BitMapper(m, bits_per_row, rows, cols)
-    % author: Shanxiang Lyu, lsx07@jnu.edu.cn
-    
-    % Check if optional inputs are provided, otherwise use default values
-    if nargin < 4
-        % Define the number of bits per row in M
-        bits_per_row = [2, 2, 2, 2, 2, 2, 2, 2];
-        
-        % Define the size of the information matrix (8x8)
-        rows = 8;
-        cols = 8;
-    end
+function M = BitMapper(m,bits_per_row,rows,cols)
 
+if nargin <4
+     % Define the number of bits per row in M
+     bits_per_row = [2, 2, 2, 2, 2, 2, 2, 2];
+     % Define the size of the information matrix (8x8) and the number of bits per row
+    rows = 8;
+    cols = 8;
+end
+
+ 
     % Initialize the information matrix M
     M = zeros(rows, cols);
 
@@ -20,7 +18,8 @@ function M = BitMapper(m, bits_per_row, rows, cols)
         num_bits = bits_per_row(row);
         for col = 1:cols
             end_idx = start_idx + num_bits - 1;
-            M(row, col) = bi2de(m(start_idx:end_idx), 'left-msb'); % Convert binary to decimal
+           % M(row, col) = bi2de(m(start_idx:end_idx), 'left-msb'); % Convert binary to decimal
+            M(row, col) = bin2dec_custom(m(start_idx:end_idx)); % Convert binary to decimal
             start_idx = end_idx + 1;
         end
     end
